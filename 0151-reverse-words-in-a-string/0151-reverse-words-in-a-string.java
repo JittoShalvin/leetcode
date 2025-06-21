202f66015251dd425d44a74b1853b62193bed014
JittoShalvin <144763234+JittoShalvin@users.noreply.github.com>
2025-06-21 18:28:57 +0530

    Time: 19 ms (5.77%), Space: 45.4 MB (14.98%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -18,7 +18,9 @@ class Solution {
                 res=w+" "+res;
             }
         }
-
+            if(res.isEmpty()){
+            return "";
+        }
     return res.substring(0,res.length()-1);
     }
 
