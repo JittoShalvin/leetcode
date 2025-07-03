9ee558602e3ff9912b059bfb2b6003c2e4524bb8
JittoShalvin <144763234+JittoShalvin@users.noreply.github.com>
2025-07-03 12:02:09 +0530

    Time: 1 ms (97.34%), Space: 45.8 MB (45.66%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.java
+++ 0605-can-place-flowers/0605-can-place-flowers.java
@@ -6,7 +6,7 @@ class Solution {
         int c=0;
         for(int i=0;i<flowerbed.length;i++){
             if(flowerbed[i]==0){
-            if(i==0||flowerbed[i-1]==0 && i==flowerbed.length||flowerbed[i+1]==0){
+            if((i==0||flowerbed[i-1]==0 )&& (i==flowerbed.length-1||flowerbed[i+1]==0)){
                 flowerbed[i]=1;
                 c++;
                 if(n==c){
