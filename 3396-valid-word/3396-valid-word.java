e8b5e3a3fbf7b2fcb923f273e3cb15022c514ed8
JittoShalvin <144763234+JittoShalvin@users.noreply.github.com>
2025-07-15 11:59:50 +0530

    Time: 1 ms (99.12%), Space: 42.5 MB (19.87%) - LeetHub

--- 3396-valid-word/3396-valid-word.java
+++ 3396-valid-word/3396-valid-word.java
@@ -7,7 +7,6 @@ class Solution {
 
         for (char ch : word.toCharArray()) {
             if (!Character.isLetterOrDigit(ch)) {
-                // If character is not a letter or digit → invalid
                 return false;
             }
 
@@ -26,9 +25,5 @@ class Solution {
     public boolean vowel(char a) {
         a = Character.toLowerCase(a);
         return (a == 'a' || a == 'e' || a == 'i' || a == 'o' || a == 'u');
-    }
-
-    public boolean num(char a) {
-        return (a >= '0' && a <= '9');
     }
 }
