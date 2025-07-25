b68f188a5c13498e3c9ac0dcefad2d36c37b5ca8
JittoShalvin <144763234+JittoShalvin@users.noreply.github.com>
2025-07-25 15:18:02 +0530

    Time: 1 ms (99.76%), Space: 45.8 MB (94.4%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,20 +1,14 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int[] dup=new int[nums.length];
         int j=0;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]!=0){
-                dup[j]=nums[i];
+        for(int num:nums){
+            if(num!=0){
+                nums[j]=num;
                 j++;
             }
         }
-        for(int i=0;i<nums.length;i++){
-            if(dup[i]==0){
-                nums[i]=0;
-            }
-            else{
-                nums[i]=dup[i];
-            }
+        for(int i=j;i<nums.length;i++){
+            nums[i]=0;
         }
     }
 }
